1b2cea225af0c3028f8d7c21944f7c977e57e29a
I033516 <I033516@INLN50851250A.apj.global.corp.sap>
2016-12-08 11:22:24 +0530

    Second commit

--- src/main/java/com/sap/lbh/FreightTenderingApplication.java
+++ src/main/java/com/sap/lbh/FreightTenderingApplication.java
@@ -2,6 +2,7 @@ package com.sap.lbh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.sap.lbh.entities.*;
 
 @SpringBootApplication
 public class FreightTenderingApplication {
